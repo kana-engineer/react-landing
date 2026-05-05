01e761071233725ccb831131fdb2d6eef963b0bd
kana-engineer <alimoovc@gmail.com>
2026-05-05 22:05:44 +0600

    add response

--- src/Components/Demo.jsx
+++ src/Components/Demo.jsx
@@ -1,9 +1,9 @@
 const Demo = () => {
     return (
         <div className="Add">
-             <div className="flex flex-col items-center justify-center mt-10 bg-[#F5F7FA] py-20">
-                <h1 className="text-6xl font-bold text-center mb-10">Pellentesque suscipit <br /> fringilla libero eu.</h1>
-                 <button className="bg-[#4CAF4F] px-3 py-2 text-white flex items-center gap-2 max-[901px]:hidden">
+             <div className="flex flex-col items-center justify-center mt-10 bg-[#F5F7FA] lg:py-20 py-15">
+                <h1 className="lg:text-6xl text-3xl font-bold text-center mb-10">Pellentesque suscipit <br /> fringilla libero eu.</h1>
+                 <button className="bg-[#4CAF4F] px-3 py-2 text-white flex items-center gap-2">
                     Get a Demo
                     <i className="bx bx-right-arrow-alt"></i>
                 </button>
