ab7f53de48df772b7e9a205e3b108de8950f83d1
kana-engineer <alimoovc@gmail.com>
2026-05-07 10:55:41 +0600

    start to footer

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Header from "./Components/Header";
 import Overlay from "./Components/Overlay";
 import Smith from "./Components/Smith";
 import Unseen from "./Components/Unseen";
+import Unseen from "./Components/Footer";
 
 export function App() {
   return (
@@ -22,6 +23,7 @@ export function App() {
       <Smith/>
       <Caring/>
       <Demo/>
+      <Footer/>
     </>
   )
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="App">
+            
+        </div>
+    );
+}
+
+export default Footer;
